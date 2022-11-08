f1eb5c5d6de96fa5af9d4a199b4e473054757176
Demetriobc <112503929+Demetriobc@users.noreply.github.com>
2022-11-08 13:11:47 -0300

    commit inicial
    primeiro commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.svg" alt="bandeira do ${player1}" />
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="bandeira do ${player2}" />
+  </li>  
+  `
+}
+let delay = -0.5
+function createCard(date, day, games) {
+  delay = delay + 0.5
+  return `
+      <div class="card" style="animation-delay: ${delay}s">
+          <h2>${date} <span>${day}</span></h2>
+          <ul>
+            ${games}
+          </ul>
+        </div>
+  `
+}
+document.querySelector(".cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brasil", "16:00", "serbia")) +
+  createCard("28/11", "segunda", createGame("brasil", "13:00", "swit")) +
+  createCard("28/11", "segunda", createGame("brasil", "16:00", "2"))
---
+++ readme.md
@@ -0,0 +1,1 @@
+Projeto feito durante o evento nlw tematico da copa da rocketseat com a finalidade de iniciar no mundo do front end, usando conceitos basicos de html, css e javascript.
